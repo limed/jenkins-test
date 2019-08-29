8da5c2af99d4f0417574c91d3e226fa862615d89
limed <limed@mozilla.com>
2019-08-28 17:02:50 -0700

    Fix yet anohter typo

--- Jenkinsfiles/util.groovy
+++ Jenkinsfiles/util.groovy
@@ -3,7 +3,7 @@ def notify_slack(Map args, credential_id='slack-hook') {
     def command = "${env.WORKSPACE}/scripts/slack-notify.sh"
     withCredentials([string(credentialsId: credential_id, variable: 'HOOK')]) {
         for (arg in args) {
-            command = += " --${arg.key} '${arg.value}'"
+            command += " --${arg.key} '${arg.value}'"
         }
         command += " --hook '${HOOK}'"
         sh command
